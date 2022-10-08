87346c33b0a0782b606744d2bf6aebf742aa3e1a
Maxime Bartha <Maxime.Bartha@gmail.com>
2022-10-09 00:01:17 +0200

    start serie4

---
+++ serie4/userInput.py
@@ -0,0 +1,26 @@
+dic = []
+tried_letters = []
+with open("words.txt") as file:
+    for line in file:
+        dic.append(line.strip())
+
+        
+def belongs_to_dictionary(word):
+    if word in dic:
+        return True
+    return False
+
+def ask_word_dictionary():
+    word = input("enter a word plz: ")
+    while not belongs_to_dictionary(word):
+        word = input("enter another word plz: ")
+    return word
+
+def ask_letter(tried_letters):
+    letter = input("enter a letter plz: ")
+    while letter in tried_letters:
+        letter = input("enter another letter plz: ")
+    return letter
+
+ask_word_dictionary()
+ask_letter(tried_letters)
